c65b1f9291b8e410406807938dbb43af3ea43860
dhq_boiler <dhq_boiler@live.jp>
2025-06-25 15:35:42 +0900

    Remove unused classes

--- app/models/annotation_converter.rb
+++
@@ -1,7 +0,0 @@
-class AnnotationConverter
-  JSON2INLINE_API = URI("https://pubannotation.org/conversions/json2inline").freeze
-
-  def to_inline(json)
-    SimpleInlineTextAnnotation.generate(JSON.parse(json))
-  end
-end
