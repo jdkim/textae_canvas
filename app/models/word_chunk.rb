9e7f651d2d4e62782d267e2a2a09e5e57baa61ce
dhq_boiler <dhq_boiler@live.jp>
2025-07-04 16:56:49 +0900

    Changed the way class methods are defined to a more appropriate writing style

--- app/models/word_chunk.rb
+++ app/models/word_chunk.rb
@@ -1,10 +1,10 @@
 class WordChunk
   # Extracts chunks of text from the words array, ensuring that each chunk does not exceed the specified window size.
-  def WordChunk.from(text, window_size)
-    words_with_newlines = WordChunk.words_with_newlines_enum(text)
+  def self.from(text, window_size: 20)
+    words_with_newlines = words_with_newlines_enum(text)
     Enumerator.new do |y|
       words_with_newlines.each_slice(window_size) do |chunk_words|
-        y.yield WordChunk.join_words_to_text(chunk_words)
+        y.yield join_words_to_text(chunk_words)
       end
     end
   end
@@ -15,7 +15,7 @@ class WordChunk
   # Normalizes line endings (\r\n to \n) and splits each line into words.
   # Empty lines are represented by a single newline character.
   # Words are collected sequentially, with newline characters added as separate elements only when lines end with newlines.
-  def WordChunk.words_with_newlines_enum(text)
+  def self.words_with_newlines_enum(text)
     Enumerator.new do |y|
       normalized_text = text.gsub(/\r\n/, "\n")
       normalized_text.each_line do |line|
@@ -31,7 +31,7 @@ class WordChunk
   end
 
   # Joins an array of words into text, handling newlines and spaces appropriately
-  def WordChunk.join_words_to_text(words)
+  def self.join_words_to_text(words)
     text = ""
     words.each do |word|
       if word == "\n"
