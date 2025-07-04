05274321fd33d9766f308a024f65387706cab23d
dhq_boiler <dhq_boiler@live.jp>
2025-07-04 11:44:13 +0900

    Extract class

--- app/models/ai_annotation.rb
+++ app/models/ai_annotation.rb
@@ -1,28 +1,6 @@
 class AiAnnotation < ApplicationRecord
   attr_accessor :text, :prompt, :token_used
 
-  FORMAT_SPECIFICATION = <<~EOS
-    Annotate the text according to the prompt with using the following syntax:
-
-    ## Annotation Format
-    - An annotation consists of two consecutive square bracket pairs:
-      - First: annotated text
-      - Second: label
-    - Example: [Annotated Text][Label]
-
-    ## Label Definition (Optional)
-    - Labels can be defined as `[Label]: URL`.
-
-    ## Escaping Metacharacters
-    - To prevent misinterpretation, escape the first `[` if it naturally occurs.
-    - Example: \[Part of][Original Text]
-
-    ## Handling Unknown Prompts
-    - If could not understand prompt, return the input text unchanged.
-
-    Output the original text with annotations.
-  EOS
-
   before_create :clean_old_annotations
   before_create :set_uuid
 
@@ -36,10 +14,7 @@ class AiAnnotation < ApplicationRecord
   end
 
   def annotate!
-    # To reduce the risk of API key leakage, API error logging is disabled by default.
-    # If you need to check the error details, enable logging by add argument `log_errors: true` like: OpenAI::Client.new(log_errors: true)
-    client = OpenAI::Client.new
-
+    openai_chat_template = OpenAiChatTemplate.new
     word_chunk = WordChunk.new
 
     # Extract text chunks using WordChunk class
@@ -47,8 +22,8 @@ class AiAnnotation < ApplicationRecord
 
     total_tokens_used, combined_result = chunks.each_with_index.reduce([ 0, "" ]) do |(tokens_sum, result), (chunk, index)|
       user_content = "#{chunk}\n\nPrompt:\n#{@prompt}"
-      user_content += "\n\n(This is part #{index + 1}. Please annotate this part only.)" if chunks.drop(1).any?
-      call_openai_chat_api(client, result, tokens_sum, user_content)
+      user_content += "\n\n(This is part #{index + 1}. Please annotate this part only.)" if chunks.take(2).size > 1
+      openai_chat_template.call(result, tokens_sum, user_content)
     end
 
     self.token_used = total_tokens_used
@@ -62,22 +37,6 @@ class AiAnnotation < ApplicationRecord
   end
 
   private
-
-  def call_openai_chat_api(client, result, tokens_sum, user_content)
-    response = client.chat(
-      parameters: {
-        model: "gpt-4o",
-        messages: [
-          { role: "system", content: FORMAT_SPECIFICATION },
-          { role: "user", content: user_content }
-        ]
-      }
-    )
-    [
-      tokens_sum.to_i + (response.dig("usage", "total_tokens") || 0).to_i,
-      result.to_s + (response.dig("choices", 0, "message", "content") || "").to_s
-    ]
-  end
 
   def clean_old_annotations
     AiAnnotation.old.destroy_all
