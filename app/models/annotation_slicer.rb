cc815ef686f9db92f54a2675e8df45ca746134b5
dhq_boiler <dhq_boiler@live.jp>
2025-07-18 16:06:40 +0900

    Rename

--- app/models/annotation_slicer.rb
+++ app/models/annotation_slicer.rb
@@ -1,14 +1,14 @@
 class AnnotationSlicer
   def initialize(original_text, original_denotations, original_relations)
-    @original_text = original_text
-    @original_denotations = original_denotations || []
-    @original_relations = original_relations || []
+    @text = original_text
+    @denotations = original_denotations || []
+    @relations = original_relations || []
   end
 
   def build_chunk_data(chunk_start, chunk_end)
     denotations = denotations_in_chunk chunk_start, chunk_end
     relations = relations_in_chunk denotations
-    chunk_text = @original_text[chunk_start...chunk_end]
+    chunk_text = @text[chunk_start...chunk_end]
     {
       "text" => chunk_text,
       "denotations" => denotations,
@@ -17,7 +17,7 @@ class AnnotationSlicer
   end
 
   def denotations_in_chunk(chunk_start, chunk_end)
-    @original_denotations.map do |d|
+    @denotations.map do |d|
       d_start = d["span"]["begin"]
       d_end = d["span"]["end"]
       if d_start >= chunk_start && d_end <= chunk_end
@@ -34,7 +34,7 @@ class AnnotationSlicer
 
   def relations_in_chunk(chunk_denotations)
     chunk_ids = chunk_denotations.map { |it| it["id"] }
-    @original_relations.each_with_object([]) do |r, arr|
+    @relations.each_with_object([]) do |r, arr|
       subj, obj = r["subj"], r["obj"]
       if chunk_ids.include?(subj) && chunk_ids.include?(obj)
         arr << r
