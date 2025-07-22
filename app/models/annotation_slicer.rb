5beadce8759e4366a876b88f5e53b23ef46a24bd
dhq_boiler <dhq_boiler@live.jp>
2025-07-22 10:45:21 +0900

    implemented AnnotationSlicer

---
+++ app/models/annotation_slicer.rb
@@ -0,0 +1,46 @@
+class AnnotationSlicer
+  def initialize(text, denotations, relations)
+    @text = text
+    @denotations = denotations || []
+    @relations = relations || []
+  end
+
+  def annotation_in(range)
+    {
+      "text" => @text[range.begin...range.end],
+      "denotations" => denotations_in(range),
+      "relations" => relations_of(denotations_in range)
+    }
+  end
+
+  def denotations_in(range)
+    @denotations.map do |denotation|
+      begin_index = denotation["span"]["begin"]
+      end_index = denotation["span"]["end"]
+      if range.cover?(begin_index..end_index)
+        {
+          "id" => denotation["id"],
+          "span" => { "begin" => begin_index - range.begin, "end" => end_index - range.begin },
+          "obj" => denotation["obj"]
+        }
+      else
+        nil
+      end
+    end.compact
+  end
+
+  def relations_of(denotations)
+    ids = denotations.map { it["id"] }
+    @relations.each_with_object([]) do |r, arr|
+      subj, obj = r["subj"], r["obj"]
+      if ids.include?(subj) && ids.include?(obj)
+        arr << r
+      elsif ids.include?(subj) || ids.include?(obj)
+        raise Exceptions::RelationCrossesChunkError, "Relation #{r.inspect} crosses chunk boundary"
+      else
+        # If neither subject nor object is in the range, skip this relation
+        next
+      end
+    end
+  end
+end
