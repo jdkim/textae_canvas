fde3de7d0a6ee5cb98bec3755d4751011de3922e
dhq_boiler <dhq_boiler@live.jp>
2025-07-24 15:33:10 +0900

    Refactor

--- app/models/annotation_merger.rb
+++ app/models/annotation_merger.rb
@@ -4,45 +4,32 @@ class AnnotationMerger
   end
 
   def merge
+    id_seq = 1
+    id_map = {}
+    offset = 0
     merged_text = ""
     merged_denotations = []
     merged_relations = []
-    id_map = {}
-    offset = 0
-    id_seq = 1
 
-    @annotations.each_with_index do |annotation, idx|
+    @annotations.each do |annotation|
       text = annotation["text"]
       denotations = annotation["denotations"] || []
       relations = annotation["relations"] || []
 
-      # If the end of the text has a period, add a space after it
-      offset += 1 if merged_text.last =~ /[.]/
-      merged_text += " " if merged_text.last =~ /[.]/
-      # Concatenate the existing text and the new text
+      if merged_text.end_with?(".")
+        merged_text += " "
+        offset += 1
+      end
       merged_text += text
-      # Assign a new id to avoid duplication of denotation ids
-      denotations.each do |denotation|
-        new_id = "T#{id_seq}"
-        id_map[denotation["id"]] = new_id
-        merged_denotations << {
-          "id" => new_id,
-          "span" => {
-            "begin" => denotation["span"]["begin"] + offset,
-            "end" => denotation["span"]["end"] + offset
-          },
-          "obj" => denotation["obj"]
-        }
-        id_seq += 1
-      end
-      relations.each do |relation|
-        subj = id_map[relation["subj"]]
-        obj = id_map[relation["obj"]]
-        # Add only if both ids are mapped
-        if subj && obj
-          merged_relations << relation.merge("subj" => subj, "obj" => obj)
-        end
-      end
+
+      # Merge denotations (also returns id_map and id_seq)
+      denotations_result = merge_denotations denotations, offset, id_seq, id_map
+      merged_denotations.concat denotations_result[:denotations]
+      id_map = denotations_result[:id_map]
+      id_seq = denotations_result[:id_seq]
+
+      # Merge relations
+      merged_relations.concat merge_relations(relations, id_map)
       offset += text.length
     end
 
@@ -51,5 +38,42 @@ class AnnotationMerger
       "denotations" => merged_denotations,
       "relations" => merged_relations
     }
+  end
+
+  private
+
+  def merge_denotations(denotations, offset, id_seq, id_map)
+    merged = []
+    denotations.each do |denotation|
+      new_id = "T#{id_seq}"
+      id_map = id_map.merge({ denotation["id"] => new_id })
+      merged << {
+        "id" => new_id,
+        "span" => {
+          "begin" => denotation["span"]["begin"] + offset,
+          "end" => denotation["span"]["end"] + offset
+        },
+        "obj" => denotation["obj"]
+      }
+      id_seq += 1
+    end
+
+    {
+      denotations: merged,
+      id_map: id_map,
+      id_seq: id_seq
+    }
+  end
+
+  def merge_relations(relations, id_map)
+    merged = []
+    relations.each do |relation|
+      subj = id_map[relation["subj"]]
+      obj = id_map[relation["obj"]]
+      if subj && obj
+        merged << relation.merge("subj" => subj, "obj" => obj)
+      end
+    end
+    merged
   end
 end
