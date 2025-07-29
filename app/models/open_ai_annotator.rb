a3c4afdb4c4f66711a4fc3ec5a36cd2120f3528b
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-07-29 20:58:20 +0900

    Log request and response of AI

--- app/models/open_ai_annotator.rb
+++ app/models/open_ai_annotator.rb
@@ -22,22 +22,28 @@ class OpenAiAnnotator
   EOS
 
   def call(user_content)
+    Rails.logger.info "Request to AI: \n===>\n#{user_content}\n===>" if Rails.env.development?
+
     # To reduce the risk of API key leakage, API error logging is disabled by default.
     # If you need to check the error details, enable logging by add argument `log_errors: true` like: OpenAI::Client.new(log_errors: true)
     client = OpenAI::Client.new
+    parameters = {
+      model: "gpt-4o",
+      messages: [
+        { role: "system", content: FORMAT_SPECIFICATION },
+        { role: "user", content: user_content }
+      ]
+    }
 
-    response = client.chat(
-      parameters: {
-        model: "gpt-4o",
-        messages: [
-          { role: "system", content: FORMAT_SPECIFICATION },
-          { role: "user", content: user_content }
-        ]
-      }
-    )
+    response = client.chat(parameters: parameters)
+    total_tokens = (response.dig("usage", "total_tokens") || 0).to_i
+    content = response.dig("choices", 0, "message", "content") || ""
+
+    Rails.logger.info "Response from AI: \n<===\n#{content}\n<===" if Rails.env.development?
+
     [
-      (response.dig("usage", "total_tokens") || 0).to_i,
-      (response.dig("choices", 0, "message", "content") || "").to_s
+      total_tokens,
+      content,
     ]
   end
 end
