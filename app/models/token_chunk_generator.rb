7226c783d32e5d8e97d93d4d7f8ada51d41071a8
dhq_boiler <dhq_boiler@live.jp>
2025-07-29 15:03:16 +0900

    Refactor

--- app/models/token_chunk_generator.rb
+++ app/models/token_chunk_generator.rb
@@ -47,7 +47,7 @@ class TokenChunkGenerator
     actual_tokens = @tokens.select { |token| chunk_begin <= token.start_offset && token.end_offset <= extended_chunk_end }
 
     if (shrink_index = find_denotation_crossing_index(chunk_begin, extended_chunk_end, actual_tokens))&.positive?
-      actual_tokens = actual_tokens.first(shrink_index)
+      actual_tokens = actual_tokens.first shrink_index
       extended_chunk_end = actual_tokens.last.end_offset if actual_tokens.any?
     end
 
