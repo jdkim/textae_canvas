12c0aa8fc9e019a13f721c94d7aa5cd3d28efba3
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-07-30 13:28:43 +0900

    Inline functions that has only one line body

--- app/models/smart_multilingual_tokenizer.rb
+++ app/models/smart_multilingual_tokenizer.rb
@@ -6,15 +6,8 @@ class SmartMultilingualTokenizer
     @client = Elasticsearch::Client.new(hosts: [ "localhost:9200" ])
   end
 
-  # Detect language and tokenize text using Elasticsearch analyzer
-  def analyze(text)
-    tokenize_with_standard_analyzer(text)
-  end
-
-  private
-
   # Tokenize text using Elasticsearch's analyze API
-  def tokenize_with_standard_analyzer(text)
+  def analyze(text)
     @client.indices.analyze(
       index: INDEX_NAME,
       body: { analyzer: "standard", text: text }
