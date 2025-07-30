c0d48cd36cb4dd5f07b40284fbe91f4c58fba5b2
dhq_boiler <dhq_boiler@live.jp>
2025-07-30 10:32:06 +0900

    Remove unused code

--- app/models/token_chunk_generator.rb
+++ app/models/token_chunk_generator.rb
@@ -116,66 +116,8 @@ class TokenChunkGenerator
     end
   end
 
-  # Decide chunk start/end and which tokens to include
-  def resolve_chunk_range_and_tokens(window_tokens)
-    chunk_begin = window_tokens.first.start_offset
-
-    extended_chunk_end = find_chunk_end_boundary @original_text, chunk_begin
-
-    actual_tokens = @tokens.select { |token| chunk_begin <= token.start_offset && token.end_offset <= extended_chunk_end }
-
-    if (shrink_index = find_denotation_crossing_index(chunk_begin, extended_chunk_end, actual_tokens))&.positive?
-      actual_tokens = actual_tokens.first shrink_index
-      extended_chunk_end = actual_tokens.last.end_offset if actual_tokens.any?
-    end
-
-    [ chunk_begin, extended_chunk_end, actual_tokens ]
-  end
-
-  # Find the end of the chunk by sentence boundary or punctuation
-  def find_chunk_end_boundary(text, current_end)
-    last_found_end = current_end
-    begin_index = current_end
-    @text = text
-
-    while current_end < text.length
-      char = text[current_end]
-      # Check for sentence-ending punctuation
-      if char =~ /[\.。！？!?]/
-        last_found_end = current_end + 1
-        current_end += 1
-      end
-      current_end += 1
-
-      # Check if window size is exceeded
-      word_count = text[begin_index..current_end].split(" ").length
-      char_count = current_end - begin_index
-      if window_unit(word_count, char_count) > @window_size
-        return last_found_end
-      end
-    end
-
-    current_end
-  end
-
   def language
     @language ||= LanguageDetectable.detect_language(@text)
-  end
-
-  # Find if any denotation crosses the chunk boundary
-  def find_denotation_crossing_index(chunk_begin, chunk_end, window_tokens)
-    return nil if window_tokens.empty?
-
-    @original_denotations.each do |d|
-      d_begin = d["span"]["begin"]
-      d_end = d["span"]["end"]
-      # If denotation starts in chunk but ends outside, find where to shrink
-      if chunk_begin <= d_begin && d_begin < chunk_end && chunk_end < d_end
-        found_index = window_tokens.find_index { |t| d_begin <= t[:start_offset] }
-        return found_index if found_index && found_index > 0
-      end
-    end
-    nil
   end
 
   # Decide window unit: char count for CJK, token count otherwise
