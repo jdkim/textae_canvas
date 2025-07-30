c1043ce3f4310578f5943af4579094307f8f3d11
dhq_boiler <dhq_boiler@live.jp>
2025-07-30 11:16:40 +0900

    Make detect_language to raise error when text is nil or empty

--- app/models/language_detectable.rb
+++ app/models/language_detectable.rb
@@ -1,6 +1,6 @@
 module LanguageDetectable
   def self.detect_language(text)
-    return "unknown" if text.nil? || text.empty?
+    raise ArgumentError "parameter 'text' is nil or empty." if text.nil? || text.empty?
     lang = CLD3::NNetLanguageIdentifier.new(0, 400).find_language(text)&.language
     case lang
     when :ja, :ko, :en
