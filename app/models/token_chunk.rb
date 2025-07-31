f942e867e1e7ede1f846a8f7c992eb19b9297936
dhq_boiler <dhq_boiler@live.jp>
2025-07-31 18:08:02 +0900

    Fix merging miss

--- app/models/token_chunk.rb
+++ app/models/token_chunk.rb
@@ -2,7 +2,7 @@ module TokenChunk
   Token = Data.define(:token, :start_offset, :end_offset, :type)
 
   # Generate token chunks from JSON data using the specified window size
-  def self.from(annotations, window_size: 20)
+  def self.from(annotations, window_size: 20, strict_mode: true)
     # window_sizeが不正ならArgumentErrorを投げる
     raise ArgumentError, "window_size must be greater than 0" if window_size <= 0
 
@@ -12,7 +12,8 @@ module TokenChunk
     # Generate chunks using the TokenChunkGenerator
     TokenChunkGenerator.new(annotations,
                             tokens,
-                            window_size)
+                            window_size,
+                            strict_mode: strict_mode)
                        .generate_chunks
   end
 end
