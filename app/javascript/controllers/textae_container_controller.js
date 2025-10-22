f3e6d117d7507f900498976722b7ec0ffab1d345
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-10-22 16:21:35 +0900

    Uses Stimulus target for editor element
    Refactors the textae-container controller to use a Stimulus target for referencing the editor element.
    
    This change improves the code's maintainability and readability by leveraging Stimulus's built-in target functionality to access the editor element, instead of relying on querySelector.

--- app/javascript/controllers/textae_container_controller.js
+++ app/javascript/controllers/textae_container_controller.js
@@ -2,10 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="textae-container"
 export default class extends Controller {
+    static targets = [ "editor" ]
+
   connect() {
-    this.element.querySelector('#ai-textae-editor').classList.add('textae-editor')
+    this.editorTarget.classList.add('textae-editor')
     const editor = window.initializeTextAEEditor()
-      .find((editor) => editor.id === 'ai-textae-editor')
+      .find((editor) => editor.id === this.editorTarget.id)
 
     editor.inspectCallback = (annotation) => {
       document.querySelector('#textae-annotation').value = JSON.stringify(annotation)
