c8477dcea6d3e6bf5e13f34322367af51ae3e7c9
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-10-23 09:42:30 +0900

    Run bin/rails app:update task

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,18 @@
 require_relative "boot"
 
+require "rails"
+# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "active_storage/engine"
-# require "action_text/engine"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,5 +35,8 @@ module TextAECanvas
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
