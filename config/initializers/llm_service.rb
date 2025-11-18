1af30f1f4379bd7c5836748e3929107f59d082e1
dhq_boiler <dhq_boiler@live.jp>
2025-11-18 11:49:03 +0900

    Run rubocop -A

--- config/initializers/llm_service.rb
+++ config/initializers/llm_service.rb
@@ -5,6 +5,5 @@
 Rails.application.configure do
   # Base URL for LLM service
   # Retrieved from environment variable LLM_SERVICE_BASE_URL, uses default value if not set
-  config.llm_service_base_url = ENV.fetch('LLM_SERVICE_BASE_URL', 'http://localhost:3000')
+  config.llm_service_base_url = ENV.fetch("LLM_SERVICE_BASE_URL", "http://localhost:3000")
 end
-
