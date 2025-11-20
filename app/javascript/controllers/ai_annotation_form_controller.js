1d614377a2a53b856a70b9a995ab466cab9de47f
dhq_boiler <dhq_boiler@live.jp>
2025-11-20 13:03:20 +0900

    Remove ||

--- app/javascript/controllers/ai_annotation_form_controller.js
+++ app/javascript/controllers/ai_annotation_form_controller.js
@@ -39,7 +39,7 @@ export default class extends Controller {
 
   #applyDefaultApiKeyAndModel() {
     const urlParams = new URLSearchParams(window.location.search)
-    const defaultApiKey = urlParams.get("api_key_uuid") || ""
+    const defaultApiKey = urlParams.get("api_key_uuid")
 
     if (defaultApiKey) {
       const apiKeySelect = this.apiKeyTarget
