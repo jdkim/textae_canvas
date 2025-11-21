cd4e53c421a865e4015d5ab297e20e2c1ed60111
dhq_boiler <dhq_boiler@live.jp>
2025-11-21 14:44:37 +0900

    Run rubocop -A

--- db/migrate/20251121000000_add_parent_to_ai_annotations.rb
+++ db/migrate/20251121000000_add_parent_to_ai_annotations.rb
@@ -3,4 +3,3 @@ class AddParentToAiAnnotations < ActiveRecord::Migration[7.1]
     add_reference :ai_annotations, :parent, foreign_key: { to_table: :ai_annotations }, index: true, null: true
   end
 end
-
